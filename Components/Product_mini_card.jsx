c56f9b9ed73986033f03e3ab03550aa837d92804
Pizatol <dev.belmonte@gmail.com>
2022-11-13 22:07:50 +0100

    mise en place de la logique du composant Rating Star

--- Components/Product_mini_card.jsx
+++ Components/Product_mini_card.jsx
@@ -4,6 +4,10 @@ import React from 'react'
 import css from "../styles/Product_mini_card.module.scss"
 import Image from 'next/image'
 
+import StarRating from './StarRating'
+
+import hd600 from '../public/assets/Pictures/inventoryImg/hd600.jpg'
+
 
 export default function Product_mini_card() {
 
@@ -12,7 +16,13 @@ export default function Product_mini_card() {
 
 	 <div className={css.global_container}>
 				<div className={css.image_container}>
-					IMAGE
+					<Image 
+						src={hd600}
+						height={300}
+						width={300}
+						layout="responsive"
+						alt="headphone"
+					/>
 				</div>
 				<div className={css.price}>
 					PRICE
@@ -23,7 +33,7 @@ export default function Product_mini_card() {
 
 				<div className={css.rating}>
 
-		
+				<StarRating/>
 
 
 
---
+++ Components/StarRating.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import css from "../styles/StarRating.module.scss";
+import { FaStar } from "react-icons/fa";
+import { COMPILER_NAMES } from "next/dist/shared/lib/constants";
+
+export default function StarRating() {
+    const [rating, setRating] = useState(null);
+    const [hover, setHover] = useState(null);
+
+    return (
+        <div>
+            {[...Array(5)].map((start, index) => {
+                const ratingValue = index + 1;
+
+                return (
+                    <label key={index}>
+                        <input
+                            className={css.input_radio}
+                            type="radio"
+                            name="rating"
+                            value={ratingValue}
+                            onClick={() => setRating(ratingValue)}
+                        />
+                        <FaStar
+                            className={css.start}
+                            key={index}
+                            size={30}
+                            color={
+                                ratingValue <= (hover || rating)
+                                    ? "#ffc107"
+                                    : "#e4e5e9"
+                            }
+                            onMouseEnter={() => setHover(ratingValue)}
+                            onMouseLeave={() => setHover(null)}
+                        />
+                    </label>
+                );
+            })}
+        </div>
+    );
+}
